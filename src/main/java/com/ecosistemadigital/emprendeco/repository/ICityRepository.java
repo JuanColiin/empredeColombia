7ee59b6ca2113f0ec73a6f1ce38e292bc9c44137
Juan Diego Peña Ceferino <juandiegopenaceferino@gmail.com>
2024-12-01 20:20:49 -0500

    Funcionan lo endpoints de Comment, pero no los de Project

--- src/main/java/com/ecosistemadigital/emprendeco/repository/ICityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ecosistemadigital.emprendeco.repository;
-
-import com.ecosistemadigital.emprendeco.entity.City;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ICityRepository extends JpaRepository<City, Long> {
-}
